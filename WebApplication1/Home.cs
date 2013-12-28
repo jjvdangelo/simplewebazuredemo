33b4c7aa8b98d6b358d49066c9543cc741ff6693
James D'Angelo <james@thegeekco.com>
2013-12-28 11:45:28 -0800

    Initial commit.

---
+++ WebApplication1/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Simple.Web;
+
+namespace WebApplication1
+{
+	[UriTemplate("/")]
+	public class Home : IGet
+	{
+		public Status Get()
+		{
+			return 200;
+		}
+	}
+}
